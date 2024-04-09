953c2127b0287e52e3bc4de6a8c99474db3ca939
backtrackrt5 <backtrackrt5@gmail.com>
2024-04-09 17:30:47 +0900

    1. RetrofitResponseData.kt  - KakaoSearchPlaceResponse, PlaceMeta, Place 추가

--- app/src/main/java/com/hsr2024/mungmungdoctortp/data/RetrofitResponseData.kt
+++ app/src/main/java/com/hsr2024/mungmungdoctortp/data/RetrofitResponseData.kt
@@ -28,3 +28,21 @@ data class LoginResponse(
     val pet_breed:String="",            // 회원 선택된 펫 견종
     val code:String=""                  // 4200 회원, 4204 회원 아님, 4203 이메일 로그인 정보 틀림, 1200 회원 추가 성공, 1201 회원 추가 실패
 )
+
+data class KakaoSearchPlaceResponse(var meta: PlaceMeta, var documents: List<Place>)
+
+data class PlaceMeta(var total_count:Int,var pageable_count:Int, var is_end:Boolean)
+
+data class Place(
+    var id:String,
+    var place_name:String,
+    var category_name:String,
+    var phone:String,
+    var address_name:String,
+    var road_address_name:String,
+    var x:String,
+    var y:String,
+    var place_url:String,
+    var distance:String
+)
+
