d5ca040b7e1c40ae1747c317745a40c657c846cd
backtrackrt5 <backtrackrt5@gmail.com>
2024-04-09 17:52:59 +0900

    1. RetrofitService.kt  - searchPlace 추가

--- app/src/main/java/com/hsr2024/mungmungdoctortp/network/RetrofitService.kt
+++ app/src/main/java/com/hsr2024/mungmungdoctortp/network/RetrofitService.kt
@@ -2,6 +2,7 @@ package com.hsr2024.mungmungdoctortp.network
 
 import com.hsr2024.mungmungdoctortp.data.AiRequest
 import com.hsr2024.mungmungdoctortp.data.AiResponses
+import com.hsr2024.mungmungdoctortp.data.KakaoSearchPlaceResponse
 import com.hsr2024.mungmungdoctortp.data.LoginData
 import com.hsr2024.mungmungdoctortp.data.LoginResponse
 import com.hsr2024.mungmungdoctortp.data.SignUpData
@@ -10,6 +11,7 @@ import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.Field
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
@@ -54,5 +56,10 @@ interface RetrofitService {
     //@POST("/sign/email_signup.php")
     //fun getSignUp(@Field("email") email:String, @Field("password") password:String, @Field("name") name:String, @Field("nickname") nickname: String) : Call<String>// POST 방식으로 전달
     //val call=retrofitService.getSignUp(email,password,name,nickName)
+
+    //카카오 로컬 검색API를 해줘~ 명세서쓰기 -- 우선 응답type을 스트링으로
+    @Headers("Authorization: KakaoAK 1339b7346295d6208b37fac2e7091de1") //REST API 키: 963ec3326effb762f45c440734baacb6
+    @GET("/v2/local/search/keyword.json?sort=distance")
+    fun searchPlace(@Query("query")query:String, @Query("x") longitude:String, @Query("y")latitude:String) : Call<KakaoSearchPlaceResponse>
 
 }
