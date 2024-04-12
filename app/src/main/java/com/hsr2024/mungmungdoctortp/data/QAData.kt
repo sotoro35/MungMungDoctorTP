9a778ff1431082e5ea36713cf08d104e10250f12
seulbi9810 <seulbi9810@gmail.com>
2024-04-12 17:20:07 +0900

    데이터 파일 수정

--- app/src/main/java/com/hsr2024/mungmungdoctortp/data/QAData.kt
+++
@@ -1,5 +0,0 @@
-package com.hsr2024.mungmungdoctortp.data
-
-data class QAData(val qa_id:String, val imgurl:String, val title:String, val nickname :String, val view_count:String, val comment_count:String)
-data class FeedData(val feed_id:String, val profile_imgurl: String, val nickname: String, val imgurl:String, val favorite:String, var isFavorite:Boolean, val comment: String, val content:String, val create_date:String )
-data class CommentData(val comment_id:String, val profile_imgurl: String, val nickname: String, val content: String, val create_date: String)
